bb1de79a996d5480aed2bedd2924b7d6d448ede7
Pravin Prakash Kumar <pprakash@pravins-mbp.corp.adobe.com>
2024-02-14 17:02:02 -0800

    update testapp gradle

--- code/places/build.gradle.kts
+++ code/places/build.gradle.kts
@@ -6,20 +6,18 @@ val mavenCoreVersion: String by project
 val mavenPlacesVersion: String by project
 
 aepLibrary {
-    namespace = "com.adobe.marketing.mobile.edge.consent"
+    namespace = "com.adobe.marketing.mobile"
     enableSpotlessPrettierForJava = true
     enableCheckStyle = true
  
     publishing {
         gitRepoName = "aepsdk-places-android"
         addCoreDependency(mavenCoreVersion)
-        addEdgeDependency(mavenPlacesVersion)
     }
 }
 
 dependencies {
     implementation("com.adobe.marketing.mobile:core:$mavenCoreVersion-SNAPSHOT")
-    implementation("com.adobe.marketing.mobile:places:$mavenPlacesVersion")
     implementation ("com.google.android.gms:play-services-location:21.1.0")
 
     // testImplementation dependencies provided by aep-library:
